8558fb4d8e98ced6c23e54499de9c0a27e950d9d
NysethTrading <kiisielo@gmail.com>
2026-03-02 10:24:34 +0100

    google analytics GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RV480KYSX0" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RV480KYSX0', { anonymize_ip: true });
+        `}} />
       </head>
       <body>{children}</body>
     </html>
